2b568f8941fa9ac657e4efa76b5dc131cd1e65aa
johnsmccoy <johnsmccoy@comcast.net>
2018-07-23 09:29:17 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Battle Time")
+
+
+let battle = 0
+
+let takeNumber = function (name) {
+    bandNumber +=1
+   return bandNumber + ". " + name
+}
+
+const train = takeNumber("Train")
+console.log(train)  // This should print "Train" in the console
+
+const drake = takeNumber("Drake")
+console.log(drake)  // This should print "Drake" in the console
